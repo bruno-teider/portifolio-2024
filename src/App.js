f8d1347bb616119e17c5e5de447ef1cbbd89cd43
bruno_teider <107257148+bruno-teider@users.noreply.github.com>
2024-08-12 16:41:30 -0300

    Change frame system is working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import './App.css';
-import MainFrame from './frames/main/MainFrame';
+import { useState } from "react";
+import "./App.css";
+import MainFrame from "./frames/main/MainFrame";
+import "./Background.css";
 
 function App() {
+  const bubbles = [1, 2, 3, 4, 5];
+  const [currentFrame, setCurrentFrame] = useState(0);
+
+  const mainToParent = (frameId) => {
+    setCurrentFrame(frameId);
+  };
+
+  const renderFrame = () => {
+    switch (currentFrame) {
+      case 0:
+        return <MainFrame mainToParent={mainToParent} />;
+      case 1:
+        return <h1>Projects</h1>;
+      case 2:
+        return <h1>About</h1>;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="App">
-      <MainFrame />
+      <div className="gradient-bg">
+        {/* The Background bubbles */}
+        <div className="gradients-container">
+          {bubbles.map((index) => (
+            <div class={`g${index} key={index}`} />
+          ))}
+        </div>
+
+        {renderFrame()}
+      </div>
     </div>
   );
 }
--- src/frames/main/MainFrame.js
+++ src/frames/main/MainFrame.js
@@ -1,21 +1,11 @@
 import React from "react";
-import "./Background.css";
 import "./MainFrame.css";
 
-const MainFrame = ({ children }) => {
-  const bubbles = [1, 2, 3, 4, 5];
+const MainFrame = ({ mainToParent }) => {
 
   return (
-    <div className="gradient-bg">
-      {/* The Background bubbles */}
-      <div className="gradients-container">
-        {bubbles.map((index) => (
-          <div class={`g${index} key={index}`} />
-        ))}
-      </div>
-
-      {/* The main paage */}
       <div className="frame">
+      {/* The main paage */}
         <div className="top-container">
           <p className="top-text">Web developer - Programmer - Designer</p>
           <hr />
@@ -23,19 +13,31 @@ const MainFrame = ({ children }) => {
         </div>
 
         <div className="option-container">
-          <div className="first-option">Projects</div>
-          <div className="second-option">About</div>
+          <div className="first-option" onClick={() => mainToParent(1)} >Projects</div>
+          <div className="second-option" onClick={() => mainToParent(2)}>About</div>
 
           <div className="bottom-options">
             <hr />
-            <a className="bottom-option-one" href="https://www.linkedin.com/in/bruno-teider/" target="_blank">Linkedin</a>
+            <a
+              className="bottom-option-one"
+              href="https://www.linkedin.com/in/bruno-teider/"
+              target="_blank"
+            >
+              Linkedin
+            </a>
             <hr className="inner-hr" />
-            <a className="bottom-option-two" href="https://github.com/bruno-teider" target="_blank">Github</a>
+            <a
+              className="bottom-option-two"
+              href="https://github.com/bruno-teider"
+              target="_blank"
+            >
+              Github
+            </a>
             <hr />
           </div>
         </div>
       </div>
-    </div>
+
   );
 };
 
