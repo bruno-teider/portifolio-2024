f5ed6ee719044dcd6caaf7fa81b9eeaf1f4485b6
bruno_teider <107257148+bruno-teider@users.noreply.github.com>
2024-08-31 16:44:20 -0300

    style: :lipstick: added the style of the color palette page

--- src/frames/colorPalette/ColorPalette.js
+++ src/frames/colorPalette/ColorPalette.js
@@ -15,16 +15,23 @@ const ColorPalette = ({ getFrameId }) => {
         <div className="text-container">
           <h1 className="date-text">08.2024</h1>
           <h2 className="github-text">Github Rep</h2>
-          <h3 className="main-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit
-            amet bibendum dui, in vulputate turpis. Curabitur feugiat
-            sollicitudin tincidunt. Aliquam vel dictum neque. Etiam pulvinar
-            vulputate ante, eu luctus enim luctus nec. Mauris vel ante lorem. Ut
-            eu nibh sodales, blandit metus porttitor, suscipit tellus. Phasellus
-            elementum ante quis metus aliquam rutrum. Fusce ornare neque felis,
-            at sagittis ligula pharetra ut suscipit mattis nunc. Nunc mollis
-            justo et nunc pretium viverra.
-          </h3>
+          <div className="text-image-wrapper">
+            <h3 className="main-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit
+              amet bibendum dui, in vulputate turpis. Curabitur feugiat
+              sollicitudin tincidunt. Aliquam vel dictum neque. Etiam pulvinar
+              vulputate ante, eu luctus enim luctus nec. Mauris vel ante lorem.
+              Ut eu nibh sodales, blandit metus porttitor, suscipit tellus.
+              Phasellus elementum ante quis metus aliquam rutrum. Fusce ornare
+              neque felis, at sagittis ligula pharetra ut suscipit mattis nunc.
+              Nunc mollis justo et nunc pretium viverra.
+            </h3>
+            <img
+              alt="image-container"
+              className="image-container"
+              src="../../assets/color-palette.gif"
+            />
+          </div>
         </div>
       </div>
 
