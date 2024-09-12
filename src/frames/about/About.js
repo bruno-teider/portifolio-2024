de8a56a35aec0d76b078a96c5dbc7997fb7a2813
bruno_teider <107257148+bruno-teider@users.noreply.github.com>
2024-09-12 07:46:08 -0300

    feat: :lipstick: finished about page

--- src/frames/about/About.js
+++ src/frames/about/About.js
@@ -19,21 +19,36 @@ const About = ({ getFrameId }) => {
             que meu primeiro "emprego" foi como soldado no exército, onde fiquei
             pouco mais de um ano. Foi uma experiência que me ensinou bastante e
             me fez crescer muito como pessoa. Atualmente, estou estagiando como
-            Desenvolvedor Front-End Mobile na Educt, utilizando React Native e
-            Typescript.
+            Desenvolvedor Front-End Mobile na startup Educt, utilizando React
+            Native e Typescript.
           </h3>
           <h3 className="main-text">
-            ㅤAlém da tecnologia, sou fã de design e sempre busco aprender mais
-            sobre o assunto em meu tempo livre. Também sou fotógrafo, realizando
-            freelances e participando ativamente do Clube de Fotografia da
-            PUCPR, onde já tive a oportunidade de expor meu trabalho em uma
-            exposição. Você pode conferir um pouco das minhas fotografias no
-            Instagram: @teider_fotos.
+            ㅤTambém sou fotógrafo, realizando freelances e participando
+            ativamente do Clube de Fotografia da PUCPR, onde já tive a
+            oportunidade de expor meu trabalho em uma exposição. Você pode
+            conferir um pouco das minhas fotografias no Instagram:
+            @teider_fotos.
           </h3>
         </div>
       </div>
 
-      <img alt="my-picture" className="my-picture" src="../../assets/eu2.JPG" />
+      <div className="image-wrapper">
+        <img
+          className="second-star"
+          src="../../assets/star.png"
+          alt="first-star"
+        />
+        <img
+          alt="my-picture"
+          className="my-picture"
+          src="../../assets/eu2.JPG"
+        />
+        <img
+          className="first-star"
+          src="../../assets/star.png"
+          alt="second-star"
+        />
+      </div>
 
       <div className="back-container-about">
         <hr />
